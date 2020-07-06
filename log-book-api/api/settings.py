9af253b7f6d447e6080d04c100e519a88af80780
Vibha Masti <vsmasti@gmail.com>
2020-07-06 08:13:42 +0000

    Removed secret key from settings

--- log-book-api/api/settings.py
+++ log-book-api/api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9nr+x&s6xyj4cty(4#k-!mpr@tszdo2+!j3_$ui2(i-g&=iw-d'
+SECRET_KEY = os.getenv(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
