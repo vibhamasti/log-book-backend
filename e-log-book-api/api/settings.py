538b4414a456451fddd82524e5018388166d266b
Vibha Masti <vsmasti@gmail.com>
2020-07-10 10:53:09 +0000

    Deploying to heroku

--- e-log-book-api/api/settings.py
+++ e-log-book-api/api/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "9nr+x&s6xyj4cty(4#k-!mpr@oszdo2+!j3_$ui5(i-g&=iw-d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
