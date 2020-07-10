6c87978551773d78b56919114309b9758382afa6
Vibha Masti <vsmasti@gmail.com>
2020-07-10 10:46:22 +0000

    Deploying to heroku

--- e-log-book-api/api/wsgi.py
+++ e-log-book-api/api/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+
+project_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(os.path.join(project_folder, "e-log-book-api/.env"))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 application = get_wsgi_application()
